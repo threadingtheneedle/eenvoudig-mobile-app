51cc058b335539cb6c7f31061520ac356cf020d0
threadingtheneedle <gino.xu@edu-kw1c.nl>
2026-07-01 21:13:10 +0200

    Made the removal page actually remove food items

--- js/removeFood.js
+++ js/removeFood.js
@@ -1,2 +1,8 @@
-//Clears everything from the localstorage
-localStorage.clear();
+//Creates const that points towards the html element with id "jsButtonAdd"
+const removeFood = document.querySelector('#delete');
+
+//Adds a function that gets activated upon being clicked on
+removeFood.addEventListener('click', () =>{
+    //Clears everything from the localstorage
+    localStorage.clear();
+});
